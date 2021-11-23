71e6de8e3e7938bd975195ad719e80bd41549923
Vitalii Kryvoshapka <v.kryvoshapka@icloud.com>
2021-11-24 01:12:42 +0200

    Added Cube view Animation

--- Amination/Amination/ViewController.swift
+++ Amination/Amination/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animatingView: UIView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    // Cube Animation
+    @IBAction func startMove() {
+        
+        let bacgroudColor = animatingView.backgroundColor
+        let alpha = animatingView.alpha
+        let center = animatingView.center
+        let transform = animatingView.transform
+        
+        UIView.animate(withDuration: 4) {
+            self.animatingView.backgroundColor = .yellow
+            self.animatingView.alpha = 0
+            self.animatingView.center = CGPoint(x: 400, y: 700)
+            self.animatingView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        } completion: { isFinished in
+            if isFinished {
+                UIView.animate(withDuration: 4, delay: 2, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
+                    self.animatingView.backgroundColor = bacgroudColor
+                    self.animatingView.alpha = alpha
+                    self.animatingView.center = center
+                    self.animatingView.transform = transform
+                }, completion: nil)
+            }
+        }
+    }
 }
 
